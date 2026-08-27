850066acf726ce1fc9aa49c35b34609fdc33a953
Obaid Awan <obaydawan@gmail.com>
2026-08-27 18:27:32 +0500

    Add .gitignore and clean up repository tracking

--- models/marts/orders_summary.sql
+++ models/marts/orders_summary.sql
@@ -3,9 +3,9 @@ select
     o.customer_id,
     o.order_status,
     o.order_purchase_timestamp,
-    sum(i.price) as total_item_price,
-    sum(i.freight_value) as total_freight,
-    sum(i.price + i.freight_value) as total_order_value
+    coalesce(sum(i.price), 0) as total_item_price,
+    coalesce(sum(i.freight_value), 0) as total_freight,
+    coalesce(sum(i.price + i.freight_value), 0) as total_order_value
 from {{ ref('stg_orders') }} o
-left join {{ ref('stg_order_items') }} i on o.order_id = i.order_id
+inner join {{ ref('stg_order_items') }} i on o.order_id = i.order_id
 group by 1, 2, 3, 4
