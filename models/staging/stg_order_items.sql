923dd249d9219b8d36201104020db04a78743caf
Obaid Awan <obaydawan@gmail.com>
2026-08-27 17:51:35 +0500

    Add staging models, orders_summary mart, and data tests, all passing successfully

---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,10 @@
+select
+    order_id,
+    order_item_id,
+    product_id,
+    seller_id,
+    shipping_limit_date::timestamp as shipping_limit_date,
+    price,
+    freight_value
+from {{ ref('olist_order_items_dataset') }}
+
